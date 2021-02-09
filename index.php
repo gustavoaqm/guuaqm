c930465451e40d6f33fa2aaac4bbbbc8f04bf654
gustavoaqm <68113672+gustavoaqm@users.noreply.github.com>
2021-02-09 16:09:47 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+	<title>Binário para Decimal</title>
+</head>
+<body>
+	<div class="form__group field">
+	  <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
+	  <label for="name" class="form__label">Name</label>
+	</div>
+</body>
+</html>
